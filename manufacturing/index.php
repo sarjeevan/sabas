c22ce22bf4f8a045027a1f172fc1bafe512b0b60
kumaran <kumaraniscool@gmail.com>
2023-08-11 15:43:28 +0530

    push

--- manufacturing/index.php
+++ manufacturing/index.php
@@ -101,7 +101,7 @@ function myFunction() {
   filter = input.value.toUpperCase();
   table = document.getElementById("myTable");
   tr = table.getElementsByTagName("tr");
-  for (i = 1; i < tr.length; i++) {
+  for (i = 0; i < tr.length; i++) {
     td = tr[i].getElementsByTagName("td")[0];
     if (td) {
       txtValue = td.textContent || td.innerText;
