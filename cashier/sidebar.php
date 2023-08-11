2588feeac87b4f313cb6f0cf3583e61177e9c362
kumaran <kumaraniscool@gmail.com>
2023-08-11 16:07:29 +0530

    cashierchange2

--- cashier/sidebar.php
+++ cashier/sidebar.php
@@ -95,7 +95,7 @@
   <!--<li class="nav-heading">Pages</li>-->
 
   <li class="nav-item">
-    <a class="nav-link collapsed" href="intend.php"  style="background-color:#402424">
+    <a class="nav-link collapsed" href="intendhistory.php"  style="background-color:#402424">
     <i class="bi bi-arrow-left-right"></i>
       <span class="text-white">Intend</span>
     </a>
